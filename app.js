116e1ec2b56587e9d33e19c385c585be2f8db134
brunodacunha <brunoc.dacunha@hotmail.com>
2016-07-01 00:14:02 -0300

    Book part1

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-(function(){
-  var gem = { name: 'Azurite', price: 2.95 };
-  var app = angular.module('gemStore', []);
-  app.controller('StoreController', function(){
-    this.product = gem;
-  });
-})();
+angular.module('app', []).controller('GuessTheNumberController', GuessTheNumberController);
+function GuessTheNumberController($scope) {
+    $scope.verifyGuess = function() {
+        $scope.deviation = $scope.original - $scope.guess;
+        $scope.noOfTries = $scope.noOfTries + 1;
+    }
+    $scope.initializeGame = function() {
+        $scope.noOfTries = 0;
+        $scope.original = Math.floor((Math.random() * 1000) + 1);
+        $scope.guess = null;
+        $scope.deviation = null;
+    }
+    $scope.initializeGame();
+}
